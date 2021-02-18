6a7ff7c1bf0bd9cdf11e37b3b6f0295ac2967c0b
dylooz <57732320+Dylooz@users.noreply.github.com>
2021-02-18 04:03:23 +0000

    Refactoring

--- utils.py
+++ utils.py
@@ -5,3 +5,8 @@ def throwErr(file, pos, msg, code):
 def readFile(filename):
   with open(filename, "r") as foo:
     return foo.read()
+
+import json
+def readInput(filename):
+	with open(filename, "r") as file:
+		return json.loads(bytearray(map(lambda x: int(x, 16), file.read(-1).split(" "))).decode("utf-8"))
